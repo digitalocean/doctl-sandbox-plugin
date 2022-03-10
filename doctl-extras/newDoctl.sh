8865df134cffe1a30e3459734e004c6c225f991e
Joshua Auerbach <jauerbach@digitalocean.com>
2022-03-10 06:09:35 -0500

    Expand the build/upload script to all OS/archs
    Using goreleaser to create the snapshot builds but zipping and
    uploading manually.

--- doctl-extras/newDoctl.sh
+++ doctl-extras/newDoctl.sh
@@ -29,28 +29,42 @@ SPACE_URL="https://$TARGET_SPACE.$DO_ENDPOINT"
 # Change this variable when local setup for s3 CLI access changes
 AWS="aws --profile do --endpoint https://$DO_ENDPOINT"
 
+# Subroutine to tar and upload one doctl binary for mac or linux
+function tar_and_upload() {
+  echo "Making tarball for $1"
+  rm -fr /tmp/sbx.tar.gz
+  tar czf /tmp/sbx.tar.gz -C dist/doctl_$1 doctl
+  echo "Uploading tarball for $1"
+  $AWS s3 cp /tmp/sbx.tar.gz "s3://$TARGET_SPACE/doctl-with-sandbox-$1.tar.gz"
+  $AWS s3api put-object-acl --bucket "$TARGET_SPACE" --key "doctl-with-sandbox-$1.tar.gz" --acl public-read
+}
+
+# Subroutine to zip and upload one doctl binary for windows
+function zip_and_upload() {
+  echo "Making zip file for $1"
+  rm -fr /tmp/sbx.zip
+  pushd dist/doctl_$1
+  zip -r /tmp/sbx.zip doctl.exe
+  popd
+  echo "Uploading zip file for $1"
+  $AWS s3 cp /tmp/sbx.zip "s3://$TARGET_SPACE/doctl-with-sandbox-$1.zip"
+  $AWS s3api put-object-acl --bucket "$TARGET_SPACE" --key "doctl-with-sandbox-$1.zip" --acl public-read
+}
+
+
 # Orient
 set -e
 SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SELFDIR/../../doctl
 
-echo "Removing old artifacts"
-rm -rf *.tar.gz
+echo "Build all the binaries"
+goreleaser build --rm-dist --snapshot
 
-echo "Building the darwin binary"
-GOOS=darwin scripts/_build.sh
-cd builds && rm -fr darwin && mkdir darwin && mv doctl_darwin* darwin/doctl && cd ..
+# Upload windows versions
+for i in windows_386 windows_amd64 windows_arm64; do
+  zip_and_upload $i
+done  
 
-echo "Building the linux binary"
-GOOS=linux scripts/_build.sh
-cd builds && rm -fr linux && mkdir linux && mv doctl_linux* linux/doctl && cd ..
-
-echo "Making the tarballs"
-tar czf doctl-sandbox-mac.tar.gz -C builds/darwin doctl
-tar czf doctl-sandbox-linux.tar.gz -C builds/linux doctl
-
-echo "Uploading"
-$AWS s3 cp doctl-sandbox-mac.tar.gz "s3://$TARGET_SPACE/doctl-with-sandbox-mac.tar.gz"
-$AWS s3api put-object-acl --bucket "$TARGET_SPACE" --key "doctl-with-sandbox-mac.tar.gz" --acl public-read
-$AWS s3 cp doctl-sandbox-linux.tar.gz "s3://$TARGET_SPACE/doctl-with-sandbox-linux.tar.gz"
-$AWS s3api put-object-acl --bucket "$TARGET_SPACE" --key "doctl-with-sandbox-linux.tar.gz" --acl public-read
+for i in darwin_amd64 darwin_arm64 linux_386 linux_amd64 linux_arm64; do
+  tar_and_upload $i
+done  
