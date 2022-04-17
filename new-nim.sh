6570daab7256aeec87f80d490b663de9ecba9d14
Joshua Auerbach <jauerbach@digitalocean.com>
2022-04-17 16:58:05 -0400

    Improvements to new-sim script.  New nim version.
    The new version fixes a bug whereby a windows initiating process could
    not successfully launch a remote build with a build.sh in it.

--- new-nim.sh
+++ new-nim.sh
@@ -2,6 +2,8 @@
 
 # Updates to the latest 'nim' and deployer
 
+rm -fr node_modules package-lock.json
 npm install @nimbella/nimbella-cli@latest --save-exact
 npm install @nimbella/nimbella-deployer@latest --save-exact
+npm install
 npx patch-package @nimbella/nimbella-cli
