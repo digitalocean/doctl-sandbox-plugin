35f443e7debc95bcbd352164d9a44eecb72143c3
Joshua Auerbach <jauerbach@digitalocean.com>
2022-04-18 05:57:08 -0400

    Script to download doctl for testing

---
+++ tests/getDoctl.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Obtains the correct doctl for the os and arch of the machine it is running on.
+# Intended for e2e sandbox plugin testing.  Assumes running under bash even on windows.
+# Note: once there is sandbox support in official doctl releases, this should download
+# those releases.  Currently, downloads from the beta staging area.
+
+# Requires the staging directory as an argument.
+# Places doctl in the 'bin' subdirectory thereof.
+[ -n "$1" ] || (echo "Missing argument" && exit 1)
+[ -z "$2" ] || (echo "Too many arguments" && exit 1)
+
+# The following is heuristic and incomplete.  A better way would be welcome.
+which uname > /dev/null || (echo "Cannot run on native windows" && exit 1)
+set -e
+UOS=$(uname -s)
+[[ $UOS == *Linux* ]] && OS=linux
+[ $UOS == Darwin ] && OS=darwin
+[ -z "$OS" ] && OS=windows
+UARCH=$(uname -m)
+if [ $UARCH == x86_64 ]; then
+  ARCH=amd64
+elif [[ $UARCH == *386* ]]; then
+  ARCH=386
+else
+  ARCH=$UARCH
+fi
+DOWNLOAD="https://do-serverless-tools.nyc3.digitaloceanspaces.com/doctl-with-sandbox-${OS}_${ARCH}.tar.gz"
+
+# Do the download
+cd "$1"
+[ -d bin ] || mkdir bin
+if [ "$OS" == windows ]; then
+  TARGET=doctl.zip
+  UNPACK=unzip
+else
+  TARGET=doctl.tar.gz
+  UNPACK="tar xzf"
+fi  
+curl "$DOWNLOAD" -o "$TARGET"
+cd bin
+$UNPACK "../$TARGET"
+echo "Doctl with sandbox has been downloaded to $1/bin"
